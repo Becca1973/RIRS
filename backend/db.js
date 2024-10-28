c68d6f26aa5e5cbba38643eca85b33bca9feee55
SaraDugi <lunarose.lr@gmail.com>
2024-10-28 14:51:44 +0100

    povezava podatkovne baze

--- backend/db.js
+++ backend/db.js
@@ -3,21 +3,26 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// objekt za povezavo z bazo
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,       // Naslov gostitelja baze podatkov (npr. localhost ali IP naslov)
+    user: process.env.DB_USER,       // Uporabniško ime za bazo podatkov
+    password: process.env.DB_PASSWORD, // Geslo za bazo podatkov
+    database: process.env.DB_NAME,   // Ime baze podatkov
 });
 
+// Funkcija za povezavo z bazo podatkov in obravnavo napak pri povezavi
 const connectDB = () => {
     db.connect((err) => {
         if (err) {
-            console.error('Database connection failed:', err.message);
+            // Izpiše sporočilo o napaki, če povezava ni uspešna
+            console.error('Povezava z bazo podatkov ni uspela:', err.message);
         } else {
-            console.log('Connected to MySQL database');
+            // Izpiše sporočilo o uspešni povezavi
+            console.log('Povezava z MySQL bazo podatkov je vzpostavljena');
         }
     });
 };
 
+// Izvozi povezavo z bazo podatkov in funkcijo connectDB za uporabo v drugih datotekah
 module.exports = { db, connectDB };
